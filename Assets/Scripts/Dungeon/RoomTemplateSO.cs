5d81887febe658ae2f2b739b6da5ac0dfcbfa1a6
joeface71 <joefacenda@yahoo.com>
2022-03-18 19:18:46 -0400

    Import dungeon room assets

--- Assets/Scripts/Dungeon/RoomTemplateSO.cs
+++ Assets/Scripts/Dungeon/RoomTemplateSO.cs
@@ -1,8 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-
 
 [CreateAssetMenu(fileName = "Room_", menuName = "Scriptable Objects/Dungeon/Room")]
 public class RoomTemplateSO : ScriptableObject
@@ -14,17 +12,18 @@ public class RoomTemplateSO : ScriptableObject
     [Space(10)]
     [Header("ROOM PREFAB")]
 
-    #endregion
+    #endregion Header ROOM PREFAB
 
     #region Tooltip
 
-    [Tooltip("The gameobject prefab for the room (this will contain all the tilemaps for the room and environment game object")]
+    [Tooltip("The gameobject prefab for the room (this will contain all the tilemaps for the room and environment game objects")]
 
     #endregion Tooltip
 
     public GameObject prefab;
 
     [HideInInspector] public GameObject previousPrefab; // this is used to regenerate the guid if the so is copied and the prefab is changed
+
 
     #region Header ROOM CONFIGURATION
 
@@ -35,8 +34,7 @@ public class RoomTemplateSO : ScriptableObject
 
     #region Tooltip
 
-    [Tooltip("the room node type SO. The  room node types correspond to the room nodes used in the room node graph.  The exceptions being with the corridors.  In the room node graph there is just one corridor " +
-        "type 'Corridor'.  For the room templates there are 2 corridor node types - CorridorNS and CorridorEW.")]
+    [Tooltip("The room node type SO. The room node types correspond to the room nodes used in the room node graph.  The exceptions being with corridors.  In the room node graph there is just one corridor type 'Corridor'.  For the room templates there are 2 corridor node types - CorridorNS and CorridorEW.")]
 
     #endregion Tooltip
 
@@ -44,8 +42,7 @@ public class RoomTemplateSO : ScriptableObject
 
     #region Tooltip
 
-    [Tooltip("If you imagine a rectangle around the room tilemap that just completely encloses it, the room lower bounds represent the bottom left corner of the rectangle.  This should be determined from the " +
-        "tilemap for the room (using the coordinate brush pointer to get the tilemap grid position for that bottom left corner (Note: This is the local tilemap position and not world position")]
+    [Tooltip("If you imagine a rectangle around the room tilemap that just completely encloses it, the room lower bounds represent the bottom left corner of that rectangle. This should be determined from the tilemap for the room (using the coordinate brush pointer to get the tilemap grid position for that bottom left corner (Note: this is the local tilemap position and NOT world position")]
 
     #endregion Tooltip
 
@@ -53,8 +50,7 @@ public class RoomTemplateSO : ScriptableObject
 
     #region Tooltip
 
-    [Tooltip("If you imagine a rectangle around the room tilemap that just completely encloses it, the room upper bounds represent the top right corner of the rectangle.This should be determined from the " +
-        "tilemap for the room (using the coordinate brush pointer to get the tilemap grid position for that bottom left corner (Note: This is the local tilemap position and not world position")]
+    [Tooltip("If you imagine a rectangle around the room tilemap that just completely encloses it, the room upper bounds represent the top right corner of that rectangle. This should be determined from the tilemap for the room (using the coordinate brush pointer to get the tilemap grid position for that top right corner (Note: this is the local tilemap position and NOT world position")]
 
     #endregion Tooltip
 
@@ -62,12 +58,11 @@ public class RoomTemplateSO : ScriptableObject
 
     #region Tooltip
 
-    [Tooltip("There should be a maximum of 4 doorways per room - one for each compass direction. These should have a constant 3 tile opening size, with the middle tile position being the doorway coordinate 'position'")]
+    [Tooltip("There should be a maximum of four doorways for a room - one for each compass direction.  These should have a consistent 3 tile opening size, with the middle tile position being the doorway coordinate 'position'")]
 
     #endregion Tooltip
 
     [SerializeField] public List<Doorway> doorwayList;
-
 
     #region Tooltip
 
@@ -80,7 +75,6 @@ public class RoomTemplateSO : ScriptableObject
     /// <summary>
     /// Returns the list of Entrances for the room template
     /// </summary>
-    /// <returns></returns>
     public List<Doorway> GetDoorwayList()
     {
         return doorwayList;
@@ -90,7 +84,7 @@ public class RoomTemplateSO : ScriptableObject
 
 #if UNITY_EDITOR
 
-    //Validate SO fields
+    // Validate SO fields
     private void OnValidate()
     {
         // Set unique GUID if empty or the prefab changes
@@ -103,12 +97,11 @@ public class RoomTemplateSO : ScriptableObject
 
         HelperUtilities.ValidateCheckEnumerableValues(this, nameof(doorwayList), doorwayList);
 
-        //Check spawn positions populated
+        // Check spawn positions populated
         HelperUtilities.ValidateCheckEnumerableValues(this, nameof(spawnPositionArray), spawnPositionArray);
     }
 
 #endif
 
     #endregion Validation
-
 }
