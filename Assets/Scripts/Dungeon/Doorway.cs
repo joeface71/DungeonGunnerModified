c7c850be672ca8c232763ed84054aa169a2cc72c
joeface71 <joefacenda@yahoo.com>
2022-03-18 07:56:07 -0400

    Store details about room tilemaps

---
+++ Assets/Scripts/Dungeon/Doorway.cs
@@ -0,0 +1,27 @@
+
+using UnityEngine;
+
+[System.Serializable]
+public class Doorway
+{
+    public Vector2Int position;
+    public Orientation orientation;
+    public GameObject doorPrefab;
+
+    #region Header
+    [Header("The upper left position to start copying from")]
+    #endregion
+    public Vector2Int doorwayStartCopyPosition;
+    #region
+    [Header("The width of tiles in the doorway to copy over")]
+    #endregion
+    public int doorwayCopyTileWidth;
+    #region Header
+    [Header("The height of tiles in the doorway to copy over")]
+    #endregion
+    public int doorwayCopyTileHeight;
+    [HideInInspector]
+    public bool isConnected = false;
+    [HideInInspector]
+    public bool isUnavailable = false;
+}
---
+++ Assets/Scripts/Enums/Enums.cs
@@ -0,0 +1,10 @@
+
+public enum Orientation
+{
+    north,
+    east,
+    south,
+    west,
+    none
+}
+
