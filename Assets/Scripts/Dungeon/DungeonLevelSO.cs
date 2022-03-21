b2a10c4227d69135c1e3ae5e9772889dddafe7b5
joeface71 <joefacenda@yahoo.com>
2022-03-21 07:43:34 -0400

    Create singleton abstract class

--- Assets/Scripts/Dungeon/DungeonLevelSO.cs
+++ Assets/Scripts/Dungeon/DungeonLevelSO.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,41 +5,51 @@ using UnityEngine;
 public class DungeonLevelSO : ScriptableObject
 {
     #region Header BASIC LEVEL DETAILS
+
     [Space(10)]
     [Header("BASIC LEVEL DETAILS")]
-    #endregion
+
+    #endregion Header BASIC LEVEL DETAILS
 
     #region Tooltip
-    [Tooltip("The name for the level")]
-    #endregion
 
+    [Tooltip("The name for the level")]
+
+    #endregion Tooltip
     public string levelName;
 
     #region Header ROOM TEMPLATES FOR LEVEL
+
     [Header("ROOM TEMPLATES FOR LEVEL")]
-    #endregion
+
+    #endregion Header ROOM TEMPLATES FOR LEVEL
 
     #region Tooltip
+
     [Tooltip("Populate the list with the room templates that you want to be part of the level.  " +
         "You need to ensure that room templates are included for all room node types that are specified in the room node graphs for the level.")]
-    #endregion
+
+    #endregion Tooltip
 
     public List<RoomTemplateSO> roomTemplateList;
 
-
     #region Header ROOM NODE GRAPHS FOR LEVEL
+
     [Space(10)]
     [Header("ROOM NODE GRAPHS FOR LEVEL")]
-    #endregion
+
+    #endregion Header ROOM NODE GRAPHS FOR LEVEL
+
     #region Tooltip
+
     [Tooltip("Poplulate this list with the room node graphs which should be randomly selected from for the level")]
-    #endregion
+
+    #endregion Tooltip
 
     public List<RoomNodeGraphSO> roomNodeGraphList;
 
-
-
     #region Validaton
+
 #if UNITY_EDITOR
 
     private void OnValidate()
@@ -146,5 +155,6 @@ public class DungeonLevelSO : ScriptableObject
     }
 
 #endif
-    #endregion
+
+    #endregion Validaton
 }
