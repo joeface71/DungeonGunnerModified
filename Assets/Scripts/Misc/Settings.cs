c8e2fb1b6fb0d2296b9336940ae32099aa8beaf8
joeface71 <joefacenda@yahoo.com>
2022-04-13 19:22:55 -0400

    Collision matrix and animation speed

--- Assets/Scripts/Misc/Settings.cs
+++ Assets/Scripts/Misc/Settings.cs
@@ -39,6 +39,7 @@ public static class Settings
     public static int rollDown = Animator.StringToHash("rollDown");
     public static int rollLeft = Animator.StringToHash("rollLeft");
     public static int rollRight = Animator.StringToHash("rollRight");
+    public static float baseSpeedForPlayerAnimations = 8f;
 
     // animator parameters - Door
     public static int open = Animator.StringToHash("open");
--- Assets/Scripts/Player/PlayerControl.cs
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -25,6 +25,13 @@ public class PlayerControl : MonoBehaviour
     private void Start()
     {
         waitForFixedUpdate = new WaitForFixedUpdate();
+
+        SetPlayerAnimationSpeed();
+    }
+
+    private void SetPlayerAnimationSpeed()
+    {
+        player.animator.speed = moveSpeed / Settings.baseSpeedForPlayerAnimations;
     }
 
     private void Update()
