9a6d9d6e81fb2aa7a3781929631b728bc56c5881
joeface71 <joefacenda@yahoo.com>
2022-04-14 13:59:36 -0400

    Object pool manager implementation

---
+++ Assets/Scripts/PoolManager/PoolManager.cs
@@ -0,0 +1,135 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class PoolManager : SingletonMonobehaviour<PoolManager>
+{
+    [Tooltip("Populate with prefabs that you want to add to the pool, and specify the number of gameobjects to be created for each")]
+    [SerializeField] private Pool[] poolArray = null;
+
+    private Transform objectPoolTransform;
+    private Dictionary<int, Queue<Component>> poolDictionary = new Dictionary<int, Queue<Component>>();
+
+    [System.Serializable]
+    public struct Pool
+    {
+        public int poolSize;
+        public GameObject prefab;
+        public string componentType;
+    }
+
+    private void Start()
+    {
+        // This singleton gameobject will be the object pool parent
+        objectPoolTransform = this.gameObject.transform;
+
+        for (int i = 0; i < poolArray.Length; i++)
+        {
+            CreatePool(poolArray[i].prefab, poolArray[i].poolSize, poolArray[i].componentType);
+        }
+
+
+    }
+
+    /// <summary>
+    /// Create the object pool with the specified prefabs and the specified pool size for each
+    /// </summary>
+    /// <param name="prefab"></param>
+    /// <param name="poolSize"></param>
+    /// <param name="componentType"></param>
+    private void CreatePool(GameObject prefab, int poolSize, string componentType)
+    {
+        int poolKey = prefab.GetInstanceID();
+
+        string prefabName = prefab.name;
+
+        GameObject parentGameObject = new GameObject(prefabName + "Anchor"); // create parent gameobject to parent the child object to
+
+        parentGameObject.transform.SetParent(objectPoolTransform);
+
+        if (!poolDictionary.ContainsKey(poolKey))
+        {
+            poolDictionary.Add(poolKey, new Queue<Component>());
+
+            for (int i = 0; i < poolSize; i++)
+            {
+                GameObject newObject = Instantiate(prefab, parentGameObject.transform) as GameObject;
+
+                newObject.SetActive(false);
+
+                poolDictionary[poolKey].Enqueue(newObject.GetComponent(Type.GetType(componentType)));
+            }
+        }
+    }
+
+    /// <summary>
+    /// Reuse a gameobject component in the pool.  prefab is the prefab gameobject containing the component.  Position is world position.  rotation should be set if gameobject needs to be rotated.
+    /// </summary>
+    /// <param name="prefab"></param>
+    /// <param name="position"></param>
+    /// <param name="rotation"></param>
+    /// <returns></returns>
+    public Component ReuseComponent(GameObject prefab, Vector3 position, Quaternion rotation)
+    {
+        int poolKey = prefab.GetInstanceID();
+
+        if (poolDictionary.ContainsKey(poolKey))
+        {
+            Component componentToReuse = GetComponentFromPool(poolKey);
+
+            ResetObject(position, rotation, componentToReuse, prefab);
+
+            return componentToReuse;
+        }
+        else
+        {
+            Debug.Log("No object pool for " + prefab);
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Get a gameobject component from the pool using the poolkey
+    /// </summary>
+    /// <param name="poolKey"></param>
+    /// <returns></returns>
+    private Component GetComponentFromPool(int poolKey)
+    {
+        Component componentToReuse = poolDictionary[poolKey].Dequeue();
+        poolDictionary[poolKey].Enqueue(componentToReuse);
+
+        if (componentToReuse.gameObject.activeSelf == true)
+        {
+            componentToReuse.gameObject.SetActive(false);
+        }
+
+        return componentToReuse;
+    }
+
+    /// <summary>
+    /// Reset the gameobject
+    /// </summary>
+    /// <param name="position"></param>
+    /// <param name="rotation"></param>
+    /// <param name="componentToReuse"></param>
+    /// <param name="prefab"></param>
+    private void ResetObject(Vector3 position, Quaternion rotation, Component componentToReuse, GameObject prefab)
+    {
+        componentToReuse.transform.position = position;
+        componentToReuse.transform.rotation = rotation;
+        componentToReuse.gameObject.transform.localScale = prefab.transform.localScale;
+    }
+
+    #region Validation
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        HelperUtilities.ValidateCheckEnumerableValues(this, nameof(poolArray), poolArray);
+    }
+#endif
+    #endregion
+
+
+}
