7e9eb9a69819a882a1128782a335c8c4b73faf4c
joeface71 <joefacenda@yahoo.com>
2022-05-08 11:54:17 -0400

    Enemy Character Prefab variants

---
+++ Assets/Scripts/Enemies/EnemyDetailsSO.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyDetails_", menuName = "Scriptable Objects/Enemy/EnemyDetails")]
+public class EnemyDetailsSO : ScriptableObject
+{
+    [Space(10)]
+    [Header("BASE ENEMY DETAILS")]
+
+    [Tooltip("The name of the enemy")]
+    public string enemyName;
+
+    [Tooltip("The prefab for the enemy")]
+    public GameObject enemyPrefab;
+
+    #region Validation
+    private void OnValidate()
+    {
+        HelperUtilities.ValidateCheckEmptyString(this, nameof(enemyName), enemyName);
+        HelperUtilities.ValidateCheckNullValue(this, nameof(enemyPrefab), enemyPrefab);
+    }
+    #endregion
+}
